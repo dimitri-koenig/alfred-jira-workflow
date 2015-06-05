08eccc6cb4eec9eea95d88e1a88cc6d1a390a434
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-05 13:03:36 +0200

    Reworked credentials system

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'useLocalKeychain' => true, // set to false if you want to setup below config data
+	'username' => '', // your jira username
+	'password' => '', // your jira password
+	'hostUrl' => '' // your jira url, ex: https://my.jira.com
+);
+
+?>
